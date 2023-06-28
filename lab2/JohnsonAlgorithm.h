792affe8436d6f2b1a9a437ba85e3291addec4e6
vovilon228 <113108239+vovilon228@users.noreply.github.com>
2023-06-28 14:47:56 +0300

    Update JohnsonAlgorithm.h

--- lab2/JohnsonAlgorithm.h
+++ lab2/JohnsonAlgorithm.h
@@ -5,18 +5,18 @@
 
 using distanceVector = std::vector<int>;
 
-distanceVector belmanFord(GraphFunctionality& graph, size_t fromVertex) throw(std::runtime_error);//функція виконує алгоритм Белмана-Форда
+distanceVector belmanFord(GraphFunctionality& graph, size_t fromVertex) throw(std::runtime_error);
 
-void relax(distanceVector& dist, GraphFunctionality& graph, size_t u, size_t v); //робить релаксацію ребра тобто виключення з циклу
+void relax(distanceVector& dist, GraphFunctionality& graph, size_t u, size_t v); 
 
-void relax(distanceVector& dist, edgesContainer& edgesWeight, size_t u, size_t v); //робить релаксацію ребра тобто виключення з циклу
+void relax(distanceVector& dist, edgesContainer& edgesWeight, size_t u, size_t v); 
 
-std::vector<distanceVector> johnsonAlgorithm(GraphFunctionality& g);//функція виконує алгоритм Джонсона 
+std::vector<distanceVector> johnsonAlgorithm(GraphFunctionality& g);
 
-void initDistanceVector(distanceVector& distance, size_t fromVertex);// вилучає ребро з циклу
+void initDistanceVector(distanceVector& distance, size_t fromVertex);
 
-std::pair<size_t, int> findMin(std::vector<bool>& in, edgesContainer& edgesWeight);//щукає мінімальне ребро
+std::pair<size_t, int> findMin(std::vector<bool>& in, edgesContainer& edgesWeight);
 
-distanceVector dijkstra(GraphFunctionality& graph, edgesContainer newEdgesWeight, size_t fromVertex);//функція викнує алгоритм Дейкстри
+distanceVector dijkstra(GraphFunctionality& graph, edgesContainer newEdgesWeight, size_t fromVertex);
 
 void initDistanceVector(distanceVector& distance, size_t fromVertex);
